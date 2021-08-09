278dd91343721070ddfcb78e00056731b5adf2bd
Vitaliy <kudrvet@gmail.com>
2021-08-09 13:23:31 +0300

    fix welcome view name

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Log;
 Route::get('/', function () {
 //    Log::debug('Here is some debug information');
     Log::debug('debug info');
-    return view('welcome there!');
+    return view('welcome');
 });
