064b6aaf7c2f64e810ca39ce7f8e2f0b6a7b029f
Vitaliy <kudrvet@gmail.com>
2021-08-09 13:05:09 +0300

    update .env.example , add config for Rollbar

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Rollbar\Laravel\RollbarServiceProvider::class,
         /*
          * Application Service Providers...
          */
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Log;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+//    Log::debug('Here is some debug information');
+    Log::debug('debug info');
     return view('welcome');
 });
