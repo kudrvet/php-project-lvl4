c7c418a0a8575faaa610e9977db37c7004a1b008
Vitaliy <kudrvet@gmail.com>
2021-08-19 19:17:14 +0300

    edit homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ "Привет ! ВЫ на главной " }}
-                </div>
+    <main class="container py-4">
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="display-4">Привет от Хекслета!</h1>
+                <p class="lead">Практические курсы по программированию</p>
+                <hr class="my-4">
+                <a class="btn btn-primary btn-lg" href="https://hexlet.io" role="button">
+                    Узнать больше            </a>
             </div>
         </div>
-    </div>
-</div>
+    </main>
 @endsection
