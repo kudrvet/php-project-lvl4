60628ff9276d301e24ada069936c5fa7563c6afb
Vitaliy <kudrvet@gmail.com>
2021-08-20 21:21:10 +0300

    update tasks index

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -29,8 +29,8 @@ class TaskController extends Controller
     {
 //        $tasks = Task::all();
         $tasks = QueryBuilder::for(Task::class)
-            ->withCasts(['status_id' => 'string','created_by_id' => 'string','assigned_to_id' => 'string'])
             ->allowedFilters(['status_id','created_by_id','assigned_to_id'])
+            ->withCasts(['status_id' => 'string','created_by_id' => 'string','assigned_to_id' => 'string'])
             ->orderBy('created_at')
             ->paginate(20);
 
